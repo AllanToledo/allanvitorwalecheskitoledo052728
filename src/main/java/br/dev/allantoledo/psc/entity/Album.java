8fdc7b60b462fc2cfa58a0706654b0cea64a269d
Allan Toledo <a.llantoledo@hotmail.com>
2026-01-24 11:53:21 -0400

    feat: Adicionado campo 'ano' a entidade Álbum

--- src/main/java/br/dev/allantoledo/psc/entity/Album.java
+++ src/main/java/br/dev/allantoledo/psc/entity/Album.java
@@ -1,10 +1,12 @@
 package br.dev.allantoledo.psc.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -13,4 +15,14 @@ import lombok.EqualsAndHashCode;
 public class Album extends GenericEntity{
     @NotBlank(message = "Nome do álbum não pode ser em branco.")
     private String name;
+    @NotNull(message = "Ano do álbum não pode ser nulo.")
+    private Integer year;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "author",
+            joinColumns = @JoinColumn(name = "id_album"),
+            inverseJoinColumns = @JoinColumn(name = "id_artist")
+    )
+    private Set<Artist> authors;
 }
---
+++ src/main/resources/db/migration/postgres/V1.2__add_year.sql
@@ -0,0 +1,4 @@
+ALTER TABLE album ADD COLUMN year INT;
+UPDATE album SET year = 9999;
+
+ALTER TABLE album ALTER COLUMN year SET NOT NULL;
