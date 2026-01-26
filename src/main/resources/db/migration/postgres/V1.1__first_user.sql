54d3877a2907e165650de7a8a7eee5a239ba6417
Allan Toledo <a.llantoledo@hotmail.com>
2026-01-26 18:18:35 -0400

    fix: Primeiro usuário com permissão de administrador

--- src/main/resources/db/migration/postgres/V1.1__first_user.sql
+++ src/main/resources/db/migration/postgres/V1.1__first_user.sql
@@ -1,2 +1,7 @@
-INSERT INTO app_user(name, email, password)
-VALUES ('admin', 'admin@admin.br', '$2y$10$IYOjqQ8Iyb1OmkjzSsaXVO.13cWMxlqFR5WuZeZQuGKPYZrqHEfgW')
+INSERT INTO app_user(name, email, password, is_admin)
+VALUES (
+'admin',
+'admin@admin.br',
+'$2y$10$IYOjqQ8Iyb1OmkjzSsaXVO.13cWMxlqFR5WuZeZQuGKPYZrqHEfgW', -- 123456
+true
+)
