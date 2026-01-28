883ef247a17208704c358eba29aef87fa6a534d3
Allan Toledo <a.llantoledo@hotmail.com>
2026-01-27 22:16:54 -0400

    fix: Ajuste no Fuso Horário

--- src/main/java/br/dev/allantoledo/psc/Application.java
+++ src/main/java/br/dev/allantoledo/psc/Application.java
@@ -1,13 +1,24 @@
 package br.dev.allantoledo.psc;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
+
+    @Value("${application.timezone:UTC}")
+    private String applicationTimeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @PostConstruct
+    public void executeAfterMain() {
+        TimeZone.setDefault(TimeZone.getTimeZone(applicationTimeZone));
+    }
 }
