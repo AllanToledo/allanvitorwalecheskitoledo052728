bc231a4de2c5e971c08b85abfbdd95f54264a78f
Allan Toledo <a.llantoledo@hotmail.com>
2026-02-03 14:47:10 -0400

    refactor: Classe movida de pacote.

--- src/main/java/br/dev/allantoledo/psc/configuration/AsyncConfiguration.java
+++ src/main/java/br/dev/allantoledo/psc/configuration/AsyncConfiguration.java
@@ -1,4 +1,4 @@
-package br.dev.allantoledo.psc.components;
+package br.dev.allantoledo.psc.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
